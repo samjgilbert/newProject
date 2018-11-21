e88e6dbe52b70569906ee0b3d5864232e117c177
Sam Gilbert <sam.gilbert@ucl.ac.uk>
2018-11-21 13:36:21 +0000

    latest

--- newProject/src/com/sam/newproject/client/NewProject.java
+++ newProject/src/com/sam/newproject/client/NewProject.java
@@ -1,14 +1,18 @@
 package com.sam.newproject.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class NewProject implements EntryPoint {
+	//First initialise the sequencePosition variable. This keeps track of where we
+	//are in the experiment, controlling the sequential ordering of events.
+	static int sequencePosition=0; 
+	
+	ParticipantInfo p = new ParticipantInfo(958);
+	
 	public void onModuleLoad() {
-		RootPanel.get().add(new Label("Hello, world."));
+		new SequenceHandler();
 	}
 }
